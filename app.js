e73c82bda744d49cdae36702c50e08646092d491
Steve Song <steve@bitfinder.co>
2017-07-26 17:01:25 +0900

    fix app.js

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const http = require('http');
 const hostname = '127.0.0.1';
 const port = 3000;
 
+console.log('hi')
+
 const server = http.createServer( (request, response) => {
     response.statusCode = 200;
     response.setHeader('Content-Type', 'text/plain');
