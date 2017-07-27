c63d72b8bee0fc59c91c14154dae99f1f8dfdb51
Steve Song <steve@bitfinder.co>
2017-07-27 15:19:26 +0900

    add Express router middleware

--- app.js
+++ app.js
@@ -4,9 +4,14 @@ const express = require('express')
 var app = express()
 const port = process.env.PORT || 8080;
 
+var bodyParser = require('body-parser');
 
+
+//Express middleware
+app.use(bodyParser.json()); // for parsing application/json
+
+//Express routing
 app.use('/static', express.static(__dirname + '/public'));
-
 
 app.get('/', function(req, res){
 	// res.render('index');
@@ -16,6 +21,25 @@ app.get('/', function(req, res){
 app.get('/api', (req, res) => {
     res.json({ fname:'Steve', lname:'Song'});
 })
+
+app.get('/users/:userType/:userId/:messageType', function(req, res){
+	var userType = req.params.userType;
+	var userId = parseInt(req.params.userId);
+	var messageType = req.params.messageType;
+
+    res.status(200);
+	var path = req.path;
+	res.locals.path = path;
+	res.json( { devInfo: { 
+		user_type: userType, 
+		user_id: userId,
+		message_type: messageType,
+		url: req.originalUrl
+	}});
+});
+
+
+
 
 
 console.log('server start!')
